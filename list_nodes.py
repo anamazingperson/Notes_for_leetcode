87add0ac056dd8b6c60f52a68177e315cd884b9b
李梓境 <2684008188@qq.com>
2025-09-17 10:09:50 +0800

    链表和数组之间的转换

--- list_nodes.py
+++ list_nodes.py
@@ -2,70 +2,78 @@
 
 # 列表：单向，局部的连接
 # 相遇节点判断，采用相等，哈希或者采用分析出终止条件。
-
-class Solution:
-    def getIntersectionNode(self, headA, headB):
-        if not headA or not headB:
-            return None
-
-        pA, pB = headA, headB
-
-        while pA != pB:
-            pA = pA.next if pA else headB
-            pB = pB.next if pB else headA
-
-        return pA
-    
-
-# 前缀树
-
-
-# 定义对应的节点
-
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.is_end = False
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.is_end = False
-
-# 对于树结构，最好不要改变根节点，重新定义变量，防止该变根节点
-
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-
-    def insert(self, word: str) -> None:
-        node = self.root
-        for char in word:
-            if char not in node.children:
-                node.children[char] = TrieNode()
-            node = node.children[char]
-        node.is_end = True
-
-    def search(self, word: str) -> bool:
-        node = self.root  # ✅ 用局部变量，不要改 self.root
-        for char in word:
-            if char not in node.children:
-                return False
-            node = node.children[char]
-        return node.is_end
-
-    def startsWith(self, prefix: str) -> bool:
-        node = self.root
-        for char in prefix:
-            if char not in node.children:
-                return False
-            node = node.children[char]
-        return True
-
-#  链表实现两数相加
+# 创建链表
 class ListNode:
-    def __init__(self, val=0, next=None):
+    def __init__(self,val,next = None):
         self.val = val
         self.next = next
+        
+def create_list(nums):
+    head = ListNode(0)
+    dirty = head
+    for num in nums:
+        node = ListNode(num)
+        head.next = node
+        head  = head.next
+    
+    return dirty.next
+
+def expand_list(head):
+    nums = []
+    while head:
+        nums.append(head.val)
+        head = head.next
+    return nums
+
+def get_cross(headA,headB):
+    Ahead = headA
+    Bhead = headB
+    while headA != headB:  # 循环的结束条件
+        if headA == headB:
+            return headA.val
+        if not headA:
+            headA= Bhead
+        if not headB:
+            headB= Ahead
+        headA = headA.next
+        headB = headB.next
+def test_create():
+    nums1 = [1,3,5,6,7,8]
+    nums2 = [-1,0,2,4,5,6,7,8]
+    headA = create_list(nums1)
+    headB = create_list(nums2)
+    headC = create_list(nums2)
+    A=headA
+    headA.next.next = headC
+    headB.next = headC
+
+    print(get_cross(A,headB))
+
+
+#  链表实现两数相加
+def add(l1:ListNode,l2:ListNode):
+    ans = 0
+    sign = 0
+    sign_last = 0
+    t = 0
+    while l1 or l2 or sign:
+        val1 = l1.val if l1 else 0
+        val2 = l2.val if l2 else 0
+        total = val1 + val2 + sign
+        sign = (total)//10
+        ans += (total%10)*10**t
+
+        # update
+        if l1:
+            l1 = l1.next
+        if l2:
+            l2 = l2.next
+        t += 1
+    return ans
+def test_add():
+    nums = [1,2,3,9,2]
+    nums2 = [1,2]
+    print(add(create_list(nums),create_list(nums2)))
 
 # 涉及到进位，链表的终止；
 class Solution:
@@ -96,22 +104,85 @@ class Solution:
 # 位操作异或，或者采用集合、哈希实现，但是需要额外空间
 
 
+def get_once(nums):
+    set_num = set(nums)
+    return 2*sum(set_num) - sum(nums)
+def test_once():
+    nums = [1,2,3,2,1]
+    print(get_once(nums))
+
 
 
 # 中位数，两个排列好的数组
 # 一般思路，两个指针，越小的移动，保留当前值和上一刻值，注意边界条件
 
+
+# 只考虑奇数，没有考虑偶数
+def find_mid(nums1,nums2):
+    m = len(nums1)
+    n = len(nums2)
+
+    stop = (m+n)//2
+    i,j = 0,0
+    pre,cur = 0,0
+    for t in range(stop + 1):
+        pre = cur
+        if i < m and (j>=n or nums1[i] < nums2[j]):
+            cur = nums1[i]
+            i += 1
+        elif j < n-1:
+            # 变量的作用其实是一致的，有些保留历史有些保留当前值
+            cur = nums2[j]
+            j += 1
+    if (m+n)%2:
+        return cur
+    else:
+        return (pre+cur)/2
+    
+
+
+def test_mid():
+    nums1 = range(10)
+    nums2=range(18,20)
+    print(find_mid(nums1,nums2))
+
+
 # 数组中出现次数大于一半的数字，投票法或者哈希遍历，计算每个的次数，投票法，同就投，不同就减。
 
+def vote(nums):
+    n = len(nums)
+    dic_nums = {}
+    for i in range(n):
+        dic_nums[nums[i]] = dic_nums.get(nums[i],0) + 1
+    ind = sorted(dic_nums,key=lambda x: dic_nums[x])
+    return ind[-1]
 
-# 乘积最大子数组，动态规划，状态怎么实现转移，需要了解其中的机理
-# 乘积可能为负数，负负得正，所以需要同时保存最大和最小值
+def vote2(nums):
+    n = len(nums)
+    condidate,count = 0,0
+    for num in nums:
+        if count ==0:
+            condidate = num
+        count += 1 if num==condidate else -1
+    
+    if nums.count(condidate) > n//2:
+        return condidate
+    else:
+        return None
+def test_vote():
+    nums = [1,2,3,3,3,4,4,4,4,4,4]
+    print(vote2(nums))
 
-# 戳气球，动态规划，把气球分为前后两段
-# 两段均采用边界来区分
+
 
 
 #链表排序，归并排序，拆分两段，保存一段，小的数字排序，，，还用用到next属性的，需要先判断current是否为空
+
+# 链表的基本操作
+# 排序
+
+
+
 
 # 二分法，基础的二分法，改进的二分法，边界变化，不仅仅是赋值mid,而是Mid的上一个或者下一个数，实现区间的搜索
 # 改进方法，可以改进判断的条件，是否取等号，来实现不同的边界条件
--- tree.py
+++ tree.py
@@ -22,28 +22,182 @@ class Solution:
             return self.ans
         return dfs(root,0)
 
+# 数的基本遍历
+# DFS三种遍历
+from collections import deque
+def buildtree_leveldorder(l):
+    if not l:
+        return []
+    root = TreeNode(l[0])
+    que = deque([root])
+    n = len(l)
+    i = 1
+    while i < n:
+        node = que.popleft()
+        left = TreeNode(l[i])
+        node.left = left
+        que.append(left)
+        i +=1
+        if i < n :
+            right= TreeNode(l[i])
+            node.right = right
+            que.append(right)
+        i +=1
+    return root
+
+def firstorder(root):
+    if not root:
+        return []
+    return [root.val] + firstorder(root.left) + firstorder(root.right)
+def midorder(root):
+    if not root:
+        return []
+    return  midorder(root.left) +[root.val] + midorder(root.right)
+def lastorder(root):
+    if not root:
+        return []
+    return  lastorder(root.left) + lastorder(root.right)+[root.val]
+
+def test_build_order():
+    root = buildtree_leveldorder([1,2,3,4,5,6])
+    l = lastorder(root)
+    print(root,l)
+
+# 复原树结构
+# 前序和中序复原树结构
+def rebuild_tree(l1,l2):
+    if not l1 or not l2:
+        return
+    value  = l1[0]
+    ind = l2.index(value)
+    root = TreeNode(value)
+    root.left = rebuild_tree(l1[1:1 + ind],l2[:ind])
+    root.right = rebuild_tree(l1[1+ind:],l2[ind+1:])
+    return root
+
+# 要复原一棵树，必须要有中序遍历
+def test_rebuild():
+    root = buildtree_leveldorder([1,2,3,4,5,6])
+    l1 = firstorder(root)
+    l2 = midorder(root)
+    root = rebuild_tree(l1,l2)
+    print(l1,firstorder(root))
+    print(l2,midorder(root))
+
+# 层序遍历
+def levelorder(root):
+    # 返回一维的
+    res = []
+    deq = deque([root])
+    while deq:
+        node = deq.popleft()
+        res.append(node.val)
+        if node.left:
+            deq.append(node.left)
+        if node.right:
+            deq.append(node.right)
+    return res
+
+def test_level():
+    true = range(15)
+    root = buildtree_leveldorder(true)
+    rebuild = levelorder(root)
+    print("true:",true,"rebuild",rebuild)
+
 # 树结构，递归的理解，路径的和，采用前缀和。
 # 树的基本遍历，  递归引进之后的代码执行顺序，对于空节点处理，那些代码怎么执行
+# 树的和，判断相连接节点的值和等于固定值的，对应的子连接数目
 
+def Count(root,targetSum):
+    dict_ = {0:1}
+    def dfs(root,nodeSum):
+        if not root:
+            return 0
+        nodeSum += root.val
+        ans = dict_.get(nodeSum - targetSum, 0) 
+        dict_[nodeSum] = dict_.get(nodeSum,0) + 1
+        ans +=dfs(root.left,nodeSum)
+        ans +=dfs(root.right,nodeSum)
+        dict_[nodeSum] -= 1
+        return ans
+    
+    return dfs(root,0)
 
-# 递归总结：
-# 大问题拆解的小问题具有相似性；
-# 递归的终止条件；
+def test_sum():
+    root = buildtree_leveldorder([1,2,3,4,5,6])
+    print(Count(root,6))
+# test_sum()
+    
 
+# 构建二叉搜索树
+# 利用二叉搜索树查询某个数
+def build_research(l):
+    if not l:
+        return
+    n = len(l)
+    root = TreeNode(l[n//2])
+    root.left = build_research(l[:n//2])
+    root.right = build_research(l[n//2+1:])
+    return root
+def find_value(root,value):
+    if not root:
+        return False
+    if root.val == value:
+        return True
+    if value > root.val:
+        return find_value(root.right,value)
+    else:
+        return find_value(root.left,value)
+def test_research():
+    l = range(200)
+    root = build_research(l)
+    print(levelorder(root))
+    import random
+    print(find_value(root,random.randint(200,500)))
 
+    
 # 树结构中的合并二叉树，首先得利用递归的思想，拆解成小问题，然后合并结果
 # 判断递归是否需要有返回值，返回值设置成什么，
 # 递归是自顶部向下部分解问题，自底向上合并结果，返回子树方便理解。
 # 递归定义好之后，功能和返回值，以及终止条件三要数
 
-
-# 树的几种遍历方法，以及通过遍历方法复原一棵树
-# 前序遍历，中序遍历，后序遍历，层次遍历，核心是递归，搞清楚怎么递归，功能，返回值，终止条件
-
+# 合并二叉树
+def merge(root1,root2):
+    if not root1 and not root2:
+        return
+    if root1 and root2:
+        root = TreeNode(root1.val + root2.val)
+        root.left = merge(root1.left,root2.left)
+        root.right = merge(root1.right,root2.right)
+    elif not root1:
+        return root2
+    elif not root2:
+        return root1
+    return root
+def test_merge():
+    l1 = range(10)
+    l2 = range(8)
+    root1 = buildtree_leveldorder(l1)
+    root2 = buildtree_leveldorder(l2)
+    print(levelorder(merge(root1,root2)))
 
 # 二叉搜索树的数量，给定n值，有多少种不同的二叉搜索树
 # 通过根节点数值分类，根据二叉搜索树的性质，得到左右子树的数值关系，从而得到状态转移方程
 
+def CountRT(n):
+    res = [0]*(n+1)
+    res[0] = 1
+    res[1] = 1
+    if n < 2:
+        return res[n]
+    for i in range(2,n+1):
+        value = 0
+        for j in range(i):
+            value += res[j]*res[i-1-j]
+        res[i] = value
+    return res[-1]
+def test_countrt():
+    print(CountRT(2))
 
 # 小偷问题，不能偷相连的节点
 # 递归的思路，偷当前节点，或者不偷当前节点
@@ -51,6 +205,44 @@ class Solution:
 # 有返回值的递归
 # 递归的终止条件，节点为空，返回0
 
+def chief_tree(root:TreeNode):
+    if not root:
+        return (0,0)
+    # 主要是局部的三点博弈
+
+    left = chief_tree(root.left)
+    right = chief_tree(root.right)
+    take = root.val + left[1] +right[1]
+    skip = max(left) + max(right)
+    return (take,skip)
+
+def test_chief():
+    l = range(4)[::-1]
+    l = [3,2,3,0,3,0,1]
+    root = buildtree_leveldorder(l)
+    print(max(chief_tree(root)))
+
+
 # 树展开为链表。
 # 递归的思路，先展开左右子树，然后把左子树接到右子树上，然后把右子树接到左子树的末端
 # 或者采用后续遍历，从尾巴开始连接，逆序连接。
+
+
+def toHead(root:TreeNode):
+    if not root:
+        return 
+    
+    toHead(root.right)
+    left = root.left
+    right = root.right
+    root.left = None
+    root.right = left
+    root.right.left =None
+    root.right.right = right
+    toHead(root.left)
+    return root
+
+
+l = range(5)
+root = buildtree_leveldorder(l)
+print(toHead(root))
